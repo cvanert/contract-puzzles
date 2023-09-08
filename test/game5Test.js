11e649de2f241d7887f74d5d93c1e04645c0b1e6
cvanert <cvanert@uwalumni.com>
2023-09-08 14:26:00 -0500

    Solved

--- test/game5Test.js
+++ test/game5Test.js
@@ -6,14 +6,34 @@ describe('Game5', function () {
     const Game = await ethers.getContractFactory('Game5');
     const game = await Game.deploy();
 
-    return { game };
+    // Create wallet with address less than threshold
+    const threshold = 0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf;
+    let address = threshold;
+    let wallet;
+    while (address >= threshold) {
+      wallet = await ethers.Wallet.createRandom();
+      address = wallet.address;
+    }
+
+    // Connect wallet to provider
+    wallet = wallet.connect(ethers.provider);
+
+    // Add ether to wallet so it can send transactions (gas)
+    const signer = await ethers.provider.getSigner();
+    await signer.sendTransaction({
+      to: wallet.address,
+      value: ethers.utils.parseEther("1"),
+    });
+
+
+    return { game, wallet };
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game, wallet } = await loadFixture(deployContractAndSetVariables);
 
     // good luck
 
-    await game.win();
+    await game.connect(wallet).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
